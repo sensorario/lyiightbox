a9779ce66f52ce6aaea7ddf6b83739c5d8e141ce
Simone <sensorario@gmail.com>
2011-09-24 18:34:52 +0200

    Added a lot of new features =) now start testing =)

--- lyiightbox/LyiightBox2.php
+++ lyiightbox/LyiightBox2.php
@@ -1,10 +1,13 @@
 <?php
 
+/* @todo use __DIR__ if php5.3 */
+include_once dirname(__FILE__) . '/ConfigWidget.php';
+
 /**
  * LyiightBox2 class file.
  *
  * @author Simone Gentili <sensorario@gmail.com>
- * @version 1.0
+ * @version 1.1
  */
 
 /**
@@ -14,67 +17,46 @@
  *
  * @author Simone Gentili <sensorario@gmail.com>
  */
-class LyiightBox2 extends CWidget {
+class LyiightBox2 extends ConfigWidget {
 
-    private $scripts = null;
-    private $css = null;
-    private $assetsFolder = null;
-    private $assetsFolderName = 'assets';
-    private $baseUrl = null;
     public $thumbnail = '';
     public $image = '';
     public $title = 'image';
     public $group = 'default';
     public $visible = true;
 
-    private function populateCssArrayFiles() {
-        $this->css = array(
-            '/css/lightbox.css'
-        );
-    }
-
-    private function populateJsarrayScripts() {
-        $this->scripts = array(
-            '/js/prototype.js',
-            '/js/scriptaculous.js?load=effects,builder',
-            '/js/lightbox.js',
-        );
-    }
-
-    private function loadAllJsScript() {
-        $this->populateJsarrayScripts();
-
-        Yii::app()->getClientScript()->registerScript('_', 'lyiightboxAssetUrl = "' . (Yii::app()->getAssetManager()->publish($this->assetsFolder)) . '";', CClientScript::POS_HEAD);
-
-        foreach ($this->scripts as $filename)
-            Yii::app()->getClientScript()->registerScriptFile($this->baseUrl . $filename, CClientScript::POS_END);
-    }
-
-    private function loadAllCssScripts() {
-        $this->populateCssArrayFiles();
-        foreach ($this->css as $filename)
-            Yii::app()->getClientScript()->registerCssFile($this->baseUrl . $filename);
-    }
-
-    private function setAssetsFolder() {
-        $this->assetsFolder = dirname(__FILE__) . DIRECTORY_SEPARATOR . $this->assetsFolderName;
-        $this->baseUrl = CHtml::asset($this->assetsFolder);
+    public function __construct() {
+        parent::__construct();
     }
 
     private function printImage() {
-        return $this->visible ? '<img src="' . $this->thumbnail . '" border="0" />' : '';
+        return $this->visible ? '<img src="'
+                . $this->thumbnail
+                . '" border="0" />' : '';
     }
 
     private function printLink() {
-        echo '<a href="' . $this->image . '" rel="lightbox[_' . $this->group . ']" title="' . $this->title . '">' . $this->printImage() . '</a>';
+        echo '<a '
+        . 'href="' . $this->image . '" '
+        . 'rel="lightbox[_' . $this->group . ']" '
+        . 'title="' . $this->title . '"'
+        . '>'
+        . $this->printImage()
+        . '</a>';
     }
 
     public function init() {
-
-        $this->setAssetsFolder();
-        $this->loadAllJsScript();
-        $this->loadAllCssScripts();
+        parent::config();
         $this->printLink();
+    }
+
+    public static function thumb(Galleryii $galleryii, $image) {
+        echo '<a '
+        . 'href="' . "{$galleryii->rootFolder}{$galleryii->baseFolder}{$galleryii->images}/{$image}" . '" '
+        . 'rel="lightbox[galleryii]" '
+        . '>'
+        . '<img src="'."{$galleryii->rootFolder}{$galleryii->baseFolder}{$galleryii->thumbnails}/{$image}".'">'
+        . '</a>';
     }
 
 }
